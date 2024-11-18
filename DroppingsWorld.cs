3c225c6a83926525fc0f0c6adb08b6534aa5a194
Rikard Karlsson <karlsson0214@gmail.com>
2024-11-18 18:53:55 +0100

    Add plums.

--- DroppingsWorld.cs
+++ DroppingsWorld.cs
@@ -5,11 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using DroppingsSolution;
 using EasyMonoGame;
+using Microsoft.Xna.Framework;
 
 namespace EasyDroppings
 {
     internal class DroppingsWorld : World
     {
+        private Random random = new Random();
         public DroppingsWorld() : base(600, 800)
         {
             // Tile background with file "bluerock" in the Content folder.
@@ -20,6 +22,17 @@ namespace EasyDroppings
             // Add the snake to the world at position (300, 700).
             // Set the snake image to the file "snake" in the Content folder.
             Add(snake, "snake", 300, 700);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            if (random.NextDouble() < gameTime.ElapsedGameTime.TotalSeconds)
+            {
+                Add(new Plum(), "plum", random.Next(0, Width), 0);
+            }
+
         }
     }
 }
